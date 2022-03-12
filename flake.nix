d116e2b370317d24aca76a10969e365824b547ff
hugosenari <hugosenari@gmail.com>
2022-03-12 03:23:56 -0300

    chore: re index tam

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = ''Tales of Maj'Eyal addon manager'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."tam-master".url = "path:./master";
+  inputs."tam-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tam-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."tam-v0_1_0".url = "path:./v0_1_0";
+  inputs."tam-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tam-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."tam-v0_1_1".url = "path:./v0_1_1";
+  inputs."tam-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tam-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."tam-v0_1_2".url = "path:./v0_1_2";
+  inputs."tam-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tam-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."tam-v0_1_3".url = "path:./v0_1_3";
+  inputs."tam-v0_1_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tam-v0_1_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."tam-v0_1_4".url = "path:./v0_1_4";
+  inputs."tam-v0_1_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tam-v0_1_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_1_3/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = ''Tales of Maj'Eyal addon manager'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-tam-v0_1_3.flake = false;
+  inputs.src-tam-v0_1_3.owner = "SolitudeSF";
+  inputs.src-tam-v0_1_3.ref   = "refs/tags/v0.1.3";
+  inputs.src-tam-v0_1_3.repo  = "tam";
+  inputs.src-tam-v0_1_3.type  = "github";
+  
+  inputs."cligen".dir   = "nimpkgs/c/cligen";
+  inputs."cligen".owner = "riinr";
+  inputs."cligen".ref   = "flake-pinning";
+  inputs."cligen".repo  = "flake-nimble";
+  inputs."cligen".type  = "github";
+  inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimquery".dir   = "nimpkgs/n/nimquery";
+  inputs."nimquery".owner = "riinr";
+  inputs."nimquery".ref   = "flake-pinning";
+  inputs."nimquery".repo  = "flake-nimble";
+  inputs."nimquery".type  = "github";
+  inputs."nimquery".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimquery".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."tiny_sqlite".dir   = "nimpkgs/t/tiny_sqlite";
+  inputs."tiny_sqlite".owner = "riinr";
+  inputs."tiny_sqlite".ref   = "flake-pinning";
+  inputs."tiny_sqlite".repo  = "flake-nimble";
+  inputs."tiny_sqlite".type  = "github";
+  inputs."tiny_sqlite".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tiny_sqlite".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-tam-v0_1_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-tam-v0_1_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
